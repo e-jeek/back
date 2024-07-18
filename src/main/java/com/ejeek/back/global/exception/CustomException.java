80782ad8f21c0cfe138b67a4e99438c237b05f7f
yeonssu <dustn7197@naver.com>
2024-07-18 10:03:28 +0900

    feat : error handling 기능 추가

---
+++ src/main/java/com/ejeek/back/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.ejeek.back.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ExceptionCode exceptionCode;
+
+    public CustomException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
