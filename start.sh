18c1b449096f0f801693e3db7e354ba7ad5275ea
zsunz <komlov@naver.com>
2024-07-19 20:17:06 +0900

    자동배포 테스트

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ec2-user/apps/spring-practice"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+PROJECT_ROOT="/home/ec2-user/apps/workoutwith"
+JAR_FILE="$PROJECT_ROOT/build/libs/back-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
