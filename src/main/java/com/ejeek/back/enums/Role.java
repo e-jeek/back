72490b8c344481fe03bb61a8e542c82334ca922b
Jsungyoung <sungyoung961@naver.com>
2024-07-22 20:59:36 +0900

    add: security, CorsConfig ,Role enum 추가 refactor: 회원 entity, controller, service, Dto 코드 수정

---
+++ src/main/java/com/ejeek/back/enums/Role.java
@@ -0,0 +1,5 @@
+package com.ejeek.back.enums;
+
+public enum Role {
+    USER,ADMIN
+}
---
+++ src/main/java/com/ejeek/back/global/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.ejeek.back.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource));
+
+        http.headers(headers -> {
+            headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
+        });
+
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/members/test").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
