6bfadaac65d6606f7cfb8455afca5fe661bd6df6
zsunz <komlov@naver.com>
2024-07-26 11:56:50 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(sudo docker container ls -q)
+JAVA_PID=$(sudo docker container ls -q --filter "ancestor=workoutwith")
 
-if [ -z $CURRENT_PID ]
+if [ -z $JAVA_PID ]
 then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+  echo "> 현재 구동중인 JAVA Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
-  sudo docker stop $CURRENT_PID
+  echo "> sudo docker stop $JAVA_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $JAVA_PID
   sleep 5
 fi
 
