bde8a20ace2ae72e08d6c7a7b07f905ff8eabd6c
yeonssu <dustn7197@naver.com>
2024-07-29 15:29:56 +0900

    refactor : 엔티티 필드 변경

--- src/main/java/com/ejeek/back/action/wakeup/WakeupLog.java
+++ src/main/java/com/ejeek/back/action/wakeup/WakeupLog.java
@@ -1,7 +1,5 @@
 package com.ejeek.back.action.wakeup;
 
-import com.ejeek.back.action.Action;
-import com.ejeek.back.image.Image;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +16,4 @@ public class WakeupLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime wakeupTime;
-    private String content;
-
 }
