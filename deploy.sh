38c25fa2dcd4eee4e8ba2467a16cb8bb46752aeb
zsunz <komlov@naver.com>
2024-07-29 14:34:20 +0900

    fix: 컨테이너 삭제

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,10 @@ else
 fi
 
 
+## 실행중이지 않은 컨테이너 삭제
+docker container prune
+
+
 ## 이전 버젼의 java Docker Image 제거
 docker images -q | grep -v -F -f <(docker ps --format "{{.Image}}" | awk -F: '{print $1}') | xargs -r docker rmi
 
