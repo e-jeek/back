77065a9b9bb1e9286541431820e1f054ac988322
zsunz <komlov@naver.com>
2024-07-29 17:49:19 +0900

    add: feedsavedto.java

--- src/main/java/com/ejeek/back/feed/dto/FeedSaveDto.java
+++ src/main/java/com/ejeek/back/feed/dto/FeedSaveDto.java
@@ -1,7 +1,39 @@
 package com.ejeek.back.feed.dto;
 
 
+import com.ejeek.back.feed.entity.Feed;
+import com.ejeek.back.hashtag.HashtagReference;
+import com.ejeek.back.image.ImageReference;
+import com.ejeek.back.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-
+@Getter
+@NoArgsConstructor
 public class FeedSaveDto {
+    private long id;
+    private Member member;
+    private String content;
+
+
+    private ImageReference.MappingType feedImage;
+    private HashtagReference.MappingType feedHashtag;
+    private long refId;
+
+    @Builder
+    public FeedSaveDto(long id, Member member, String content, ImageReference.MappingType feedImage, HashtagReference.MappingType feedHashtag, long refId) {
+        this.id = id;
+        this.member = member;
+        this.content = content;
+        this.feedImage = feedImage;
+        this.feedHashtag = feedHashtag;
+        this.refId = refId;
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/ejeek/back/feed/entity/Feed.java
+++ src/main/java/com/ejeek/back/feed/entity/Feed.java
@@ -3,10 +3,12 @@ package com.ejeek.back.feed.entity;
 import com.ejeek.back.global.audit.Timestamped;
 import com.ejeek.back.global.referable.HashtagReferable;
 import com.ejeek.back.global.referable.ImageReferable;
+import com.ejeek.back.hashtag.Hashtag;
 import com.ejeek.back.hashtag.HashtagReference;
 import com.ejeek.back.image.ImageReference;
 import com.ejeek.back.member.Member;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -37,4 +39,14 @@ public class Feed extends Timestamped implements ImageReferable, HashtagReferabl
     public Long getRefId() {
         return this.id;
     }
+
+    @Builder
+    public Feed(Long id, Member member, String content, ImageReference.MappingType feedImage, HashtagReference.MappingType feedHashtag, long refId ) {
+        this.id = id;
+        this.member = member;
+        this.content = content;
+
+    }
+
+
 }
