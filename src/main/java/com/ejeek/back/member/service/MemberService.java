2af139365bb061fb401ab5ba4e0a8ab070950d38
Jsungyoung <sungyoung961@naver.com>
2024-08-11 15:18:06 +0900

    refactor: Dto 분리 / mapper 사용

--- src/main/java/com/ejeek/back/member/service/MemberService.java
+++ src/main/java/com/ejeek/back/member/service/MemberService.java
@@ -1,10 +1,11 @@
 package com.ejeek.back.member.service;
 
+
 import com.ejeek.back.global.exception.CustomException;
 import com.ejeek.back.global.exception.ExceptionCode;
 import com.ejeek.back.global.jwt.dto.TokenDto;
 import com.ejeek.back.global.jwt.provider.TokenProvider;
-import com.ejeek.back.member.dto.LoginDto;
+import com.ejeek.back.member.MemberMapper;
 import com.ejeek.back.member.dto.MemberDto;
 import com.ejeek.back.member.entity.Member;
 import com.ejeek.back.member.repository.MemberRepository;
@@ -21,37 +22,41 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberMapper memberMapper;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
 
-    public Member createMember(MemberDto memberDto) {
-        validateDuplicateMember(memberDto.getEmail());
-        Member member = Member.createMember(memberDto);
-        member.setEncryptedPassword(passwordEncoder.encode(member.getPassword()));
+    public MemberDto.SimpleResponse createMember(MemberDto.SignupRequest request) {
+        validateDuplicateMember(request.getEmail());
+        Member member = memberMapper.toEntity(request);
 
-        return memberRepository.save(member);
+        member.updateEncryptedPassword(passwordEncoder.encode(member.getPassword()));
+        member.updateStatus();
+        member.updateRole();
+
+        return memberMapper.memberToSimpleResponseDto(memberRepository.save(member));
     }
-
 
     private void validateDuplicateMember(String email){
         Optional<Member> findMember = memberRepository.findByEmail(email);
+
         if(findMember.isPresent()){
             throw new CustomException(ExceptionCode.MEMBER_EMAIL_EXIST);
         }
     }
 
-    public TokenDto loginMember(LoginDto loginDto) {
-        Member member = getMemberByEmail(loginDto.getEmail());
+    public TokenDto.Response loginMember(MemberDto.LoginRequest request) {
+        Member member = getMemberByEmail(request.getEmail());
 
-        if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())){
+        if (!passwordEncoder.matches(request.getPassword(), member.getPassword())){
             throw new CustomException(ExceptionCode.MEMBER_PASSWORD_INCORRECT);
         }
 
-        return tokenProvider.generateToken(member);
+        return tokenProvider.generateToken(memberMapper.memberToSimpleResponseDto(member));
     }
 
-    private Member getMemberByEmail(String email){
+    public Member getMemberByEmail(String email){
 
         return memberRepository.findByEmail(email).orElseThrow(
                 () -> new CustomException(ExceptionCode.MEMBER_NOT_EXIST));
