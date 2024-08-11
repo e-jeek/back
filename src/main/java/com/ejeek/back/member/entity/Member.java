7099c50003436b5525eb4dc11d8f2aee05ac471a
Jsungyoung <sungyoung961@naver.com>
2024-08-11 15:21:19 +0900

    refactor: field Validation / createMember메소드 MemberMapper 사용으로 제거

--- src/main/java/com/ejeek/back/member/entity/Member.java
+++ src/main/java/com/ejeek/back/member/entity/Member.java
@@ -2,7 +2,6 @@ package com.ejeek.back.member.entity;
 
 import com.ejeek.back.global.referable.ImageReferable;
 import com.ejeek.back.image.ImageReference;
-import com.ejeek.back.member.dto.MemberDto;
 import com.ejeek.back.enums.Gender;
 import com.ejeek.back.enums.MemberStatus;
 import com.ejeek.back.enums.Role;
@@ -15,20 +14,30 @@ import java.time.LocalDate;
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends Timestamped implements ImageReferable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private MemberStatus status = MemberStatus.ACTIVE;
-    //    @ElementCollection(fetch = FetchType.EAGER)
+    private MemberStatus status;
+
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 
@@ -40,7 +49,11 @@ public class Member extends Timestamped implements ImageReferable {
 
     private Integer height;
     private Integer weight;
+
+    @Column(nullable = false)
     private Boolean policy;
+
+    @Column(nullable = false)
     private Boolean marketing;
 
     @Override
@@ -53,27 +66,14 @@ public class Member extends Timestamped implements ImageReferable {
         return this.id;
     }
 
-    public static Member createMember(MemberDto dto) {
-        Member member = new Member();
-
-        member.email = dto.getEmail();
-        member.password = dto.getPassword();
-        member.name = dto.getName();
-        member.nickname = dto.getNickname();
-        member.status = dto.getStatus();
-        member.role = Role.USER;
-        member.content = dto.getContent();
-        member.birth = dto.getBirth();
-        member.gender = dto.getGender();
-        member.height = dto.getHeight();
-        member.weight = dto.getWeight();
-        member.policy = dto.getPolicy();
-        member.marketing = dto.getMarketing();
-
-        return member;
-    }
-
-    public void setEncryptedPassword(String password) {
+    public void updateEncryptedPassword(String password) {
         this.password = password;
     }
+    public void updateStatus() {
+        this.status = MemberStatus.ACTIVE;
+    }
+    public void updateRole() {
+        this.role = Role.USER;
+    }
+
 }
