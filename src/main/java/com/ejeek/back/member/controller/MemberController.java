1979979afb93bed6462038afe88c26d0c7500339
Jsungyoung <sungyoung961@naver.com>
2024-09-08 18:19:12 +0900

    feat: 이메일, 닉네임 중복검사 / 회원삭제 기능 추가

--- src/main/java/com/ejeek/back/member/controller/MemberController.java
+++ src/main/java/com/ejeek/back/member/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.ejeek.back.member.controller;
 import com.ejeek.back.global.jwt.dto.TokenDto;
 import com.ejeek.back.global.utils.UriCreator;
 import com.ejeek.back.member.dto.MemberDto;
+import com.ejeek.back.member.entity.Member;
 import com.ejeek.back.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @RestController
@@ -30,6 +31,16 @@ public class MemberController {
         return ResponseEntity.created(UriCreator.createURI(createdMember.getId())).body(createdMember);
     }
 
+    @GetMapping("/email")
+    public ResponseEntity<?> checkEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(memberService.checkEmail(email));
+    }
+
+    @GetMapping("/nickname")
+    public ResponseEntity<?> checkNickname(@RequestParam("nickname") String nickname) {
+        return ResponseEntity.ok(memberService.checkNickname(nickname));
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> authorize(@Valid @RequestBody MemberDto.LoginRequest request) {
 
@@ -40,5 +51,12 @@ public class MemberController {
         httpHeaders.add("REFRESH_TOKEN", "Bearer " + response.getRefreshToken());
 
         return new ResponseEntity<>("ok", httpHeaders, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{Id}")
+    public ResponseEntity<?> deleteMember(@AuthenticationPrincipal Member member, @PathVariable(value = "Id") Long id) {
+        memberService.deleteMember(id, member);
+
+        return ResponseEntity.noContent().build();
     }
 }
