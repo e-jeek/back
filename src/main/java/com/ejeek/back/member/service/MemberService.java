6b256e742d21dab212241218f68a0afa3c5eade1
Jsungyoung <sungyoung961@naver.com>
2024-09-08 18:20:00 +0900

    feat: 이메일, 닉네임 중복검사/ 회원탈퇴 / 탈퇴한 유저 로그인 검증 기능 추가

--- src/main/java/com/ejeek/back/member/service/MemberService.java
+++ src/main/java/com/ejeek/back/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.ejeek.back.member.service;
 
 
+import com.ejeek.back.enums.MemberStatus;
 import com.ejeek.back.global.exception.CustomException;
 import com.ejeek.back.global.exception.ExceptionCode;
 import com.ejeek.back.global.jwt.dto.TokenDto;
@@ -28,27 +29,41 @@ public class MemberService {
     private final TokenProvider tokenProvider;
 
     public MemberDto.SimpleResponse createMember(MemberDto.SignupRequest request) {
-        validateDuplicateMember(request.getEmail());
         Member member = memberMapper.toEntity(request);
 
         member.updateEncryptedPassword(passwordEncoder.encode(member.getPassword()));
-        member.updateStatus();
+        member.updateStatus(MemberStatus.ACTIVE);
         member.updateRole();
 
         return memberMapper.memberToSimpleResponseDto(memberRepository.save(member));
     }
 
-    private void validateDuplicateMember(String email){
-        Optional<Member> findMember = memberRepository.findByEmail(email);
+    public String checkEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
 
-        if(findMember.isPresent()){
+        if (member.isPresent()) {
             throw new CustomException(ExceptionCode.MEMBER_EMAIL_EXIST);
         }
+
+        return "사용 가능한 이메일 입니다.";
+    }
+
+    public String checkNickname(String nickname) {
+        Optional<Member> member = memberRepository.findByNickname(nickname);
+
+        if (member.isPresent()) {
+            throw new CustomException(ExceptionCode.MEMBER_EMAIL_EXIST);
+        }
+
+        return "사용 가능한 닉네임 입니다.";
     }
 
     public TokenDto.Response loginMember(MemberDto.LoginRequest request) {
         Member member = getMemberByEmail(request.getEmail());
 
+        if(member.getStatus().equals(MemberStatus.DELETE)) {
+            throw new CustomException(ExceptionCode.DELETE_MEMBER);
+        }
         if (!passwordEncoder.matches(request.getPassword(), member.getPassword())){
             throw new CustomException(ExceptionCode.MEMBER_PASSWORD_INCORRECT);
         }
@@ -60,6 +75,27 @@ public class MemberService {
 
         return memberRepository.findByEmail(email).orElseThrow(
                 () -> new CustomException(ExceptionCode.MEMBER_NOT_EXIST));
+    }
+
+    public void deleteMember(Long id, Member member){
+        Member findMember = getMemberById(id);
+
+        verifyMember(member, findMember);
+
+        member.updateStatus(MemberStatus.DELETE);
+
+        memberRepository.save(member);
+    }
+
+    public Member getMemberById(Long id){
+        return memberRepository.findById(id).orElseThrow(
+                () -> new CustomException(ExceptionCode.MEMBER_NOT_EXIST));
+    }
+
+    public void verifyMember(Member member, Member findMember) {
+        if (!member.getEmail().equals(findMember.getEmail())) {
+            throw new CustomException(ExceptionCode.MEMBER_NOT_SAME);
+        }
     }
 
 }
