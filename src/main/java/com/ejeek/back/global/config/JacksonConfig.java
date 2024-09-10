b023a6d3f59980027a681794a7441417e766e064
huiju0502 <yhj960502@naver.com>
2024-09-10 16:10:29 +0900

    feat: JavaTimeModule 추가

---
+++ src/main/java/com/ejeek/back/global/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package com.ejeek.back.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
